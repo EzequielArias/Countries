e5715841cc088e4a874e562cd3467789a84d53d3
EzequielArias <ezequielariasdev@gmail.com>
2023-03-14 16:04:59 -0300

    Detailling

--- front/src/pages/Landing/Landing.jsx
+++ front/src/pages/Landing/Landing.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import '../Landing/Landing.css'
 import { Link } from 'react-router-dom'
-import wallpaper from '../../assets/mapaMundi.jpg'
 
 const Landing = () => {
   return (
     <div className='initial-container'>
-        <img src={wallpaper} alt='backgroundYummy'/>
-        <h1>Henry Countries</h1>
+        <div className='title-container'>
+          <h1>Henry Countries</h1>
         <Link to='/home' className='ov-btn-grow-ellipse'>Continuar</Link>
+        </div>
     </div>
   )
 }
